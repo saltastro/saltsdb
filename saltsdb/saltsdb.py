057dbfe5a4c43699cd035dc637d50babed9faf9a
Steve Crawford <crawfordsm@gmail.com>
2018-01-09 18:01:43 +0200

    adding connection

--- saltsdb/saltsdb.py
+++ saltsdb/saltsdb.py
@@ -1,4 +1,34 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
-from sqlalchemy import create_engine
+import sqlalchemy
 
+__all__ =['SALTSdb']
+
+
+class SALTSdb:
+    """SALTSdb is an interface to the SALT mysql database and specifically
+      simplifies the steps of returning objects from a call to the database
+
+      Parameters
+      ----------
+      host: string
+           host name of mysql database
+
+      dbname: string
+           name of mysql database
+
+      user: string
+           user for database
+
+      passwd: string
+           password of user for mysql database
+
+      port: int
+           Port for connecting to the database
+    """
+
+    def __init__(self, host, dbname, user, passwd, port):
+        sdb = 'mysql+mysqlconnector://{}:{}@{}:{}/{}'.format(user, passwd, host,
+                                                      port, dbname)
+        self.sdb = sqlalchemy.create_engine(sdb)
+ 
